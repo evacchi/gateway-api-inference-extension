df23c5abdc7a302bd58026ff11dea17980d3434b
Edoardo Vacchi <evacchi@users.noreply.github.com>
2026-01-07 10:23:44 +0100

    doc
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- pkg/bbr/datastore/datastore.go
+++ pkg/bbr/datastore/datastore.go
@@ -33,7 +33,7 @@ const (
 	adaptersKey  = "adapters"
 )
 
-// The bbr datastore stores a mapping between a LoRA adapter and its corresponding base model.
+// Datastore stores a mapping between a LoRA adapter and its corresponding base model.
 // Each ConfigMap object stores a mapping between base model and a set of LoRA adapters and datastore translates this into an in-memory cache.
 // In case the number of LoRA adapters is extremely large, it's possible to use multiple ConfigMap objects, each storing a slice of the mapping.
 // Base models are stored in a separate hash map, due to the fact that the same base model can have multiple configmap objects
